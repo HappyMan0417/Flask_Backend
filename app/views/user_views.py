e7a3c9a70ddf734698398f5527a009ed8651a7e9
HappyMan0417 <happinessman0417@skiff.com>
2023-12-25 09:49:05 -0800

    update and fix

--- app/views/user_views.py
+++ app/views/user_views.py
@@ -1,5 +1,5 @@
 from flask import jsonify, Blueprint, request
-from app.models.user_models import get_data, create_data
+from app.models.user_models import get_data, create_data, update_data_by_id, delete_data_by_id
 example_blueprint = Blueprint('example', __name__)
 
 @example_blueprint.route('/getdata', methods=['GET'])
@@ -21,3 +21,26 @@ def create():
     create_data(content)
 
     return {"message": "Successfully created!"}, 201
+
+@example_blueprint.route('/updatedata/<userid>', methods=['PUT'])
+def update_data(userid):
+
+    content = request.json
+    # your creation logic goes here...
+
+    print(content)
+    result = update_data_by_id(userid, content)
+
+    if result is None:  # if update_data returns None, user was not found
+        return {"error": "User not found", "code": 404}, 404
+    else:
+        return {"message": "Successfully updated!", "user": str(result), "code": 200}, 200  # Ensure your User model has a __str__ method
+
+@example_blueprint.route('/deletedata/<int:userid>', methods=['DELETE'])
+def delete_data(userid):
+    result = delete_data_by_id(userid)
+    if result is None:  # if delete_data returns None, user was not found
+        return {"error": "User not found"}, 404
+    else:
+        return {"message": "Successfully deleted!"}, 200
+
